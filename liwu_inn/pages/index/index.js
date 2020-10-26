9910e8d93fcc76efa59ac94cab58927573d74585
liguoqiang <331258747@qq.com>
2020-10-26 16:48:41 +0800

    首页

--- liwu_inn/pages/index/index.js
+++ liwu_inn/pages/index/index.js
@@ -4,17 +4,19 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   },
+
+  
+
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      // url: '../logs/logs'
+  btnClick: function() {
+    wx.switchTab({
+      url: '../inn/inn'
+      
     })
   },
+  
   onLoad: function () {
     
   },
--- liwu_inn/pages/my/my.js
+++ liwu_inn/pages/my/my.js
@@ -1,4 +1,4 @@
-// pages/index/logs.js
+// pages/my/my.js
 Page({
 
   /**
